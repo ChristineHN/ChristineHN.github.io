7b3dbe8416561c2600b192d68839fe5645a8132e
christineHN <hnoh3@hawk.iit.edu>
2025-03-02 18:28:57 -0600

    adding labels to each exercises

--- Lab3/hnoh-lab3.js
+++ Lab3/hnoh-lab3.js
@@ -15,6 +15,7 @@ function minMaxAverage(arr) {
     const avg = tot / arr.length;
     console.log(`Total Numbers: ${arr.length}, Min Value: ${arr[0]}, Max Value: ${arr[arr.length - 1]}, Average: ${avg}}`)
 }
+console.log(`\nExercise #1 - find min, max, and avg`);
 minMaxAverage([2, 5, 23, 6, 9, 4, 30, 1]);
 minMaxAverage([20, 15, 3, 2, 76, 3]);
 minMaxAverage([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
@@ -39,19 +40,18 @@ function countVowels(str) {
     const vowel = count > 1 ? "vowels" : "vowel";
     console.log(`${str} : ${count} ${vowel}`);
 }
+console.log(`\nExercise #2 - count vowels in the string`);
 countVowels("Winter");
 countVowels("");
 countVowels("Information Technology Management");
 countVowels("COFFEE");
 
 //Exercise #3
-/*o Write a function named `sortNumbers` that takes an array of numbers as the only parameter and returns the array sorted from smallest to largest.
-o Use console.log and invocations your sortNumbers function to print three test arrays of your choice to the console. Each test line should output to the console the original array and the sorted array.
- EXAMPLE: Original Array: [9, 4, 6, 2] output Sorted Array: [2, 4, 6, 9] */
 function sortNumbers(arr) {
     arr.sort((a, b) => a - b);
     console.log(arr);
 }
+console.log(`\nExercise #3 - sort numbers from smallest to largest`);
 sortNumbers([9, 4, 6, 2]);
 sortNumbers([1, 11, 22, 33, 111, 99, 1, 12, 13]);
 sortNumbers([]);
@@ -63,7 +63,7 @@ function celsiusToFahrenheit(temp) {
     const f = (temp * 9 / 5) + 32;
     console.log(`${temp} Celsius = ${f} Fahrenheit`);
 }
-
+console.log(`\nExercise #4 - convert Celsius to Fahrenheit`);
 celsiusToFahrenheit(30);
 celsiusToFahrenheit(0);
 celsiusToFahrenheit(12);
@@ -78,5 +78,6 @@ function sortPeople(ppl) {
         console.log(`${ppl[i].name} is ${ppl[i].age} and from ${ppl[i].city}`);
     }
 }
+console.log(`\nExercise #5 - sort people by age`);
 sortPeople([{ name: 'Michael', age: 23, city: 'Chicago' }, { name: 'Jim', age: 32, city: 'New York' }, { name: 'Joe', age: 52, city: 'St. Louis' }, { name: 'Terry', age: 12, city: 'Des Plaines' }, { name: 'Richard', age: 132, city: 'Miami' }]);
 sortPeople([{ name: 'James', age: 10, city: 'Los Angeles' }, { name: 'Mary', age: 9, city: 'Houston' }, { name: 'John', age: 8, city: 'Phoenix' }, { name: 'Emily', age: 7, city: 'Denver' }, { name: 'William', age: 6, city: 'Boston' }, { name: 'Olivia', age: 5, city: 'Orlando' }, { name: 'Daniel', age: 4, city: 'Austin' }]);
